096b26788ebbb7694d7910976bdb99d6195a9e30
Your Name <you@example.com>
2020-10-16 21:20:47 +0700

    Updates

---
+++ precache-manifest.5f27a4ad50c6a2667c2db7db743b4ff3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b4204cd0a1dfad138b304e98225944",
+    "url": "/E-shop/index.html"
+  },
+  {
+    "revision": "ca47b2b6f3357dc916f4",
+    "url": "/E-shop/static/css/2.4b370061.chunk.css"
+  },
+  {
+    "revision": "5ac94428c81c19212d24",
+    "url": "/E-shop/static/css/main.c8f41a0f.chunk.css"
+  },
+  {
+    "revision": "ca47b2b6f3357dc916f4",
+    "url": "/E-shop/static/js/2.1569352b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/E-shop/static/js/2.1569352b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac94428c81c19212d24",
+    "url": "/E-shop/static/js/main.4ef8f392.chunk.js"
+  },
+  {
+    "revision": "4795b7bce40c62c5ebed",
+    "url": "/E-shop/static/js/runtime-main.e2b260d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/E-shop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/E-shop/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/E-shop/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/E-shop/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/E-shop/static/media/slick.f97e3bbf.svg"
+  }
+]);
